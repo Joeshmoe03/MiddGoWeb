aee3497fa3dc8fc87e977832684e48b4c4c5e4fc
Joeshmoe03 <81983616+Joeshmoe03@users.noreply.github.com>
2022-05-07 19:19:35 -0400

    new push

--- WebScrapProj/WebScrape.py
+++ WebScrapProj/WebScrape.py
@@ -1,7 +1,7 @@
+#These are all of our project dependencies so far
 from bs4 import BeautifulSoup
 import pandas
-
-#These are all of our project dependencies so far
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -16,23 +16,18 @@ class Scrapify():
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
         self.link = link
         self.driver.get(self.link)
-        self.links = []
+        self.templist = []
 
     def formatPage(self, parser):
         self.content = self.driver.page_source
         self.soup = BeautifulSoup(self.content, str(parser))
 
-    def searchHyperlink(self):
-        for i, link in enumerate(self.soup.findAll("a")):
-            self.links.append(dict(i, link.get("href")))
-        return self.links
-    
+    def filter
 
 
-#textelems = driver.
     def close(self):
         self.driver.quit()
 
 page = Scrapify("http://go.middlebury.edu/gotionary.php")
 page.formatPage("html5lib")
-print(page.searchHyperlink()[0:100])
+print(page)
