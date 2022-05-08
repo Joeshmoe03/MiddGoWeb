481192afa9d3495d3c2919a862c432ce45a6b4a8
Joeshmoe03 <81983616+Joeshmoe03@users.noreply.github.com>
2022-05-07 23:52:34 -0400

    new version with more txt and html reading functionality

--- WebScrapProj/WebScrape.py
+++ WebScrapProj/WebScrape.py
@@ -3,20 +3,35 @@ from bs4 import BeautifulSoup
 import pandas
 import json
 import requests
+import urllib.request
 
 class Scrapify():
     def __init__(self, link):
         self.link = link
         self.r = requests.get(self.link)
+        self.webUrl = urllib.request.urlopen(self.link)
 
     def makeTXT(self):
-        with open("scrapedata.txt", "w") as file:
-            file.write(self.r.text)
-        return file
-    
+        with open("scrapedata.txt", "w") as self.filewritten:
+            self.filewritten.write(self.r.text)
+
+    def readHTMLrequestSuccess(self):
+        return self.webUrl.getcode()
+
+    def readHTML(self):
+        try:
+            self.readHTMLrequestSuccess()
+            self.data = self.webUrl.read()
+        except:
+            assert ConnectionError
+
+    def parseHTML(self, *attrs):
+        self.parsedHTML = BeautifulSoup(self.data, "html5lib")
+        self.parsedHTML
 
 HTMLfile = Scrapify("http://go.middlebury.edu/gotionary.php")
 TXTpage = HTMLfile.makeTXT()
-
+HTMLfile.readHTML()
+HTMLfile.parseHTML()
 
 
