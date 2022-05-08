7b7b8a63e925910bd55a7401fe2cd480e772d630
Joeshmoe03 <81983616+Joeshmoe03@users.noreply.github.com>
2022-05-08 02:27:15 -0400

    Functional recursive calls for parsing for single basic attribute is possible

--- WebScrapProj/WebScrape.py
+++ WebScrapProj/WebScrape.py
@@ -1,15 +1,18 @@
 #These are all of our project dependencies so far
 from bs4 import BeautifulSoup
+from lockfile import Error
 import pandas
 import json
 import requests
 import urllib.request
 
 class Scrapify():
-    def __init__(self, link):
+    def __init__(self, link, output = None):
         self.link = link
         self.r = requests.get(self.link)
         self.webUrl = urllib.request.urlopen(self.link)
+        self.output = output
+        self.readHTML()
 
     def makeTXT(self):
         with open("scrapedata.txt", "w") as self.filewritten:
@@ -20,18 +23,27 @@ class Scrapify():
 
     def readHTML(self):
         try:
-            self.readHTMLrequestSuccess()
+            assert(self.readHTMLrequestSuccess() == 200) # revise to see if correctly handles request validation
             self.data = self.webUrl.read()
         except:
             assert ConnectionError
 
-    def parseHTML(self, *attrs):
-        self.parsedHTML = BeautifulSoup(self.data, "html5lib")
-        self.parsedHTML
+    def parseAllHTML(self, attr):
+        parsedHTML = BeautifulSoup(self.data, "html5lib")
+        self.output = parsedHTML.findAll(str(attr))[13:14] #delete the 13:14 this is just for testing
+        return Scrapify(self.link, self.output)
+
+    def parseHTML(self, attr):
+        parsedHTML = BeautifulSoup(self.data, "html5lib")
+        output = parsedHTML.find(str(attr))
+        return Scrapify(self.link, output)
+
+    def __str__(self):
+        return str(self.output)
+ 
 
 HTMLfile = Scrapify("http://go.middlebury.edu/gotionary.php")
 TXTpage = HTMLfile.makeTXT()
-HTMLfile.readHTML()
-HTMLfile.parseHTML()
-
-
+output = HTMLfile.parseHTML('p')
+print(output)
+print(output.parseHTML('b'))
